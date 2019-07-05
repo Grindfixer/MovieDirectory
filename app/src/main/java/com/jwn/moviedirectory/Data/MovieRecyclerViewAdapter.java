c84bc9bf3c18ded63b75b7b48579d84e00c41cd8
jwn812 <john.nicholson812@gmail.com>
2019-07-04 20:22:14 -0400

    Ready for vid 159

---
+++ app/src/main/java/com/jwn/moviedirectory/Data/MovieRecyclerViewAdapter.java
@@ -0,0 +1,92 @@
+package com.jwn.moviedirectory.Data;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jwn.moviedirectory.Model.Movie;
+import com.jwn.moviedirectory.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieRecyclerViewAdapter.ViewHolder>{
+
+    private Context context;
+    private List<Movie> movieList;
+
+    public MovieRecyclerViewAdapter(Context context, List<Movie> movies){
+        this.context = context;
+        movieList = movies;
+    }
+
+    @NonNull
+    @Override
+    public MovieRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_row, parent, false);
+
+
+        return new ViewHolder(view, context);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieRecyclerViewAdapter.ViewHolder holder, int position) {
+        Movie movie = movieList.get(position);
+        String posterLink = movie.getPoster();
+
+        holder.title.setText(movie.getTitle());
+        holder.type.setText(movie.getMovieType());
+
+        Picasso.get().load(posterLink).placeholder(android.R.drawable.ic_btn_speak_now).into(holder.poster);
+
+        holder.year.setText(movie.getYear());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+            TextView title;
+            TextView year;
+            TextView type;
+            ImageView poster;
+
+        public ViewHolder(@NonNull View itemView, Context ctx) {
+            super(itemView);
+            context = ctx;
+
+            title = itemView.findViewById(R.id.movieTitleID);
+            poster = itemView.findViewById(R.id.movieImageID);
+            year = itemView.findViewById(R.id.movieReleaseDateID);
+            type = itemView.findViewById(R.id.movieCatID);
+
+                // make row clickable
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Toast.makeText(context,"Row Tapped", Toast.LENGTH_LONG).show();
+
+                }
+            });
+
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+}
